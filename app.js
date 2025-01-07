da72810e37481c36002c6c5fe1c4fa76a9420744
Default-st <stnshsrivastava@gmail.com>
2025-01-08 00:40:55 +0530

    Episode 2 commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const grandchild1 = React.createElement("h1", {}, "I am grandchild 1");
 const grandchild2 = React.createElement("h2", {}, "I am grandchild 2");
 
 const child = React.createElement("div", {}, [grandchild1, grandchild2]);
 const parent = React.createElement("div", { id: "heading" }, child);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const divRoot = document.getElementById("root");
+
+const root = createRoot(divRoot);
 root.render(parent);
